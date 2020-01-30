ae1c4c35473fd38d366606f6594db9f0b744d7bb
Sokolov-IM <56031152+Sokolov-IM@users.noreply.github.com>
2020-01-30 16:21:40 +0500

    Update spam_classifier.py

--- classifier/application/spam_classifier.py
+++ classifier/application/spam_classifier.py
@@ -1,54 +1,47 @@
 import re
 import numpy as np
+import pandas as pd
+
 pA = 0 # вероятность спама в обучающей выборке
 pNotA = 0 # вероятность не спама в обучающей выборке
 SPAM = 1
 NOT_SPAM = 0
-Z = 1 # коэффициент размытия для сглаживания по Лаппласу
+Z = 0.1 # коэффициент размытия для сглаживания по Лаппласу
 trainPositive, trainNegative = {}, {}
 positive_total, negative_total, total = 0, 0, 0
 
-train_data = [
-    ['Купите новое чистящее средство', SPAM],
-    ['Купи мою новую книгу', SPAM],
-    ['Подари себе новый телефон', SPAM],
-    ['Добро пожаловать и купите новый телевизор', SPAM],
-    ['Привет давно не виделись', NOT_SPAM],
-    ['Довезем до аэропорта из пригорода всего за 399 рублей', SPAM],
-    ['Добро пожаловать в Мой Круг', NOT_SPAM],
-    ['Я все еще жду документы', NOT_SPAM],
-    ['Приглашаем на конференцию Data Science', NOT_SPAM],
-    ['Потерял твой телефон напомни', NOT_SPAM],
-    ['Порадуй своего питомца новым костюмом', SPAM]
-]
+data = pd.read_csv('D:/Ivan/pt/spam_or_not_spam.csv')
+data.dropna(inplace=True)
 
-validation_data = [
-    ['Купите три по цене двух', SPAM],
-    ['Поздравляю, вы выиграли телевизор', SPAM],
-    ['Здраствуйте, у меня для вас новое средство для похудения', SPAM],
-    ['Добрый день! Прошу согласовать и выполнить перенос загрузок.', NOT_SPAM],
-    ['Привет. Подскажи по ошибке.', NOT_SPAM],
-    ['Купи вечером молоко', NOT_SPAM],
-    ['Поздравляю, вы приглашены на конференцию', NOT_SPAM],
-    ['Книга ждет вас в почтовом отделении', NOT_SPAM],
-    ['Здраствуйте, у меня для вас отличная новость, вы выиграли 1000000 рублей', SPAM],
-    ['Уважаемые сотрудники! В санаторий имеются диагностические и лечебные путевки.', NOT_SPAM],
-    ['Теперь вы включены в группу "Совет - посетители" SharePoint', NOT_SPAM],
-    ['Сломался телефон, ремонт всего за 500 рублей', SPAM]
-]
+# оставим только слова в тексте и ограничим длинну слов 3-15 символов
+data['email'] = data['email'].apply(lambda text: re.sub(r'\b\w{1, 3}\b', '', text))
+data['email'] = data['email'].apply(lambda text: re.sub(r'\b\w{15, }\b', '', text))
+
+spamTextCount = data[data['label'] == SPAM]['label'].sum()
+
+spam = data[data['label'] == 1]
+ham = data[data['label'] == 0]
+
+Sep = 0.9
+Sep_spam = round(len(spam)*Sep)
+Sep_ham = round(len(ham)*Sep)
+
+train_data = spam[:][:Sep_spam].append(ham[:][:Sep_ham], ignore_index=True)
+validation_data = spam[:][Sep_spam:].append(ham[:][Sep_ham:], ignore_index=True)
 
 def train():
     # Считаем pA и pNotA
     # Считаем частоты каждого слова с помощью calculate_word_frequencies()
-    global pA, pNotA, train_data, total
-    spamTextCount = 0
-    for body, label in train_data:
-        calculate_word_frequencies(body, label)
-        if label == SPAM:
-            spamTextCount += 1
+    global pA, pNotA, data, total
 
-    total = positive_total + negative_total
-    pA = spamTextCount/len(train_data)
+    for i, row in train_data.iterrows():
+        body = row['email']
+        label = row['label']
+        calculate_word_frequencies(body, label)
+
+    total = len(trainPositive) + len(trainNegative)
+
+    pA = spamTextCount/len(data)
     pNotA = 1 - pA
     # будем использовать логарифм от вероятностей, так как будут очень малые значения и округлим до 2-х знаков
     pA = round(np.log(pA), 2)
@@ -67,12 +60,15 @@ def calculate_word_frequencies(body, label):
 def calculate_P_Bi_A(word, label):
     # P(Bi|A) - вероятность встретить слово
     # применим размытие по Лаппласу для исключения нулевой вероятности новых слов
+    # Z - коэффициент размытия
     # total - общее количество уникальных слов использованное для обучения модели
     if label == SPAM:
     # будем использовать логарифм от вероятностей, так как будут очень малые значения и округлим до 2-х знаков
-        return round(np.log((trainPositive.get(word, 0) + Z) / (len(trainPositive) + Z*total)), 2)
+        #return round(np.log((trainPositive.get(word, 0) + Z) / (positive_total + Z*total)), 2) # лучший скор при Z = 0.1, но ответы: 1/3
+        return round(np.log((trainPositive.get(word, 0) + Z) / len(trainPositive)), 2) # скор хуже, но ответы 2/3
     else:
-        return round(np.log((trainNegative.get(word, 0) + Z) / (len(trainNegative) + Z*total)), 2)
+        #return round(np.log((trainNegative.get(word, 0) + Z) / (negative_total + Z*total)), 2)
+        return round(np.log((trainNegative.get(word, 0) + Z) / len(trainNegative)), 2)
 
 def calculate_P_B_A(text, label):
     # P(B|A) - вероятность встретить текст
@@ -82,40 +78,40 @@ def calculate_P_B_A(text, label):
         P_B_A = P_B_A + calculate_P_Bi_A(word, label)
     return P_B_A
 
-def classify(email):
+def classify(text):
     # Ответ: True, если спам. False, если не спам
-    # Избавляемся от знаков препинания в тексте
-    text = re.sub(r"[^A-Za-zА-Яа-я0-9 ]+", '', email)
-    #print('P_B_A(SPAM):',calculate_P_B_A(text, SPAM))
-    #print('P_B_A(NOT_SPAM):', calculate_P_B_A(text, NOT_SPAM))
-    if (pA + calculate_P_B_A(text, SPAM)) > (pNotA + calculate_P_B_A(text, NOT_SPAM)):
-        return True
+    # оставим слова в тексте, все цифры заменим на NUMBER
+
+    text = re.sub(r'\b\w{1, 3}\b', '', text)
+    text = re.sub(r'\b\w{15, }\b', '', text)
+    text = re.sub(r'\d', 'NUMBER ', text)
+
+    spam_log = pA + calculate_P_B_A(text, SPAM)
+    not_spam_log = pNotA + calculate_P_B_A(text, NOT_SPAM)
+
+    P_spam = round(100/(1 + np.exp(not_spam_log - spam_log)), 2)
+    P_ham = round(100/(1 + np.exp(spam_log - not_spam_log)), 2)
+
+    #print('P_spam:', P_spam)
+    #print('P_ham:', P_ham)
+
+    #if spam_log > not_spam_log:
+    if P_spam > P_ham:
+        return 1
     else:
-        return False
+        return 0
 
 def validation(validation_data):
     train()
-    prediction = []
 
-    for text, label in validation_data:
-        prediction[text] = classify(text)
+    validation_data['predict'] = validation_data['email'].apply(lambda x: classify(x))
 
-    TP, TN, FP, FN = 0, 0, 0, 0
+    TP = len(validation_data[(validation_data['predict']==SPAM) & (validation_data['label']==SPAM)])
+    FP = len(validation_data[(validation_data['predict']==SPAM) & (validation_data['label']==NOT_SPAM)])
+    TN = len(validation_data[(validation_data['predict']==NOT_SPAM) & (validation_data['label']==NOT_SPAM)])
+    FN = len(validation_data[(validation_data['predict']==NOT_SPAM) & (validation_data['label']==SPAM)])
 
-    # в prediction метки принимают значения True и False
-    for i, text in enumerate(prediction):
-        if text[1]:
-            if (validation_data[i][1] == SPAM):
-                TP += 1
-            else:
-                FP += 1
-        else:
-            if(validation_data[i][1] == NOT_SPAM):
-                TN += 1
-            else:
-                FN += 1
-
-    confusion_matrix = np.array([[TN, FN],[FP, TP]])
+    confusion_matrix = np.array([[TN, FP],[FN, TP]])
     accuracy = round((TP+TN)/(TP+TN+FP+FN), 2)
     recall = round(TP/(TP+FN), 2)
     precision = round(TP/(TP+FP), 2)
@@ -129,7 +125,11 @@ def validation(validation_data):
 
 #train()
 
-#result = classify('Купите три по цене двух')
-#print ('result:', result)
+#letter1 = 'Hi, My name is Warren E. Buffett an American business magnate, investor and philanthropist. am the most successful investor in the world. I believe strongly in‘giving while living’ I had one idea that never changed in my mind? that you should use your wealth to help people and i have decided to give {$1,500,000.00} One Million Five Hundred Thousand United Dollars, to randomly selected individuals worldwide. On receipt of this email, you should count yourself as the lucky individual. Your email address was chosen online while searching at random. Kindly get back to me at your earliest convenience before i travel to japan for my treatment , so I know your email address is valid. Thank you for accepting our offer, we are indeed grateful You Can Google my name for more information: God bless you. Best Regard Mr.Warren E. Buffett Billionaire investor !'
+#letter2 = 'Hi guys I want to build a website like REDACTED and I wanted to get your perspective of whether that site is good from the users\' perspective before I go ahead and build something similar. I think that the design of the site is very modern and nice but I am not sure how people would react to a similar site? I look forward to your feedback. Many thanks!'
+#letter3 = 'As a result of your application for the position of Data Engineer, I would like to invite you to attend an interview on May 30, at 9 a.m. at our office in Washington, DC. You will have an interview with the department manager, Moris Peterson. The interview will last about 45 minutes. If the date or time of the interview is inconvenient, please contact me by phone or email to arrange another appointment. We look forward to seeing you.'
+#print ('result1:', classify(letter1))
+#print ('result2:', classify(letter2))
+#print ('result3:', classify(letter3))
 
-validation(validation_data)
+#validation(validation_data)
