93f5b5f379495d034a95c6109a28238ccd66809a
Sokolov-IM <56031152+Sokolov-IM@users.noreply.github.com>
2020-01-31 11:10:58 +0500

    Update run.py

--- classifier/run.py
+++ classifier/run.py
@@ -1,5 +1,8 @@
 # classifier/run.py
 from application import app
 from application.spam_classifier import train
+import os
 
+data_path = os.path.join(app.root_path, 'spam_or_not_spam.csv')
+get_data(data_path)
 train()
