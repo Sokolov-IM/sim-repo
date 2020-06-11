feb6b1698948bcf22b23332455df138c67ae73b8
Sokolov-IM <56031152+Sokolov-IM@users.noreply.github.com>
2020-06-11 19:45:00 +0500

    Add files via upload

--- Дипломный проект/server/server.py
+++ Дипломный проект/server/server.py
@@ -1,6 +1,11 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template
+import os
+import pandas as pd
+from house_prediction.application.house_predict import load_models, get_prediction
 
 app = Flask(__name__)
+data_path = os.path.join(app.root_path, 'models')
+load_models(data_path)
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
@@ -9,26 +14,29 @@ def form():
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.form
-    features = {}
-    for item in data:
-        features[item]=data[item]
+    data_dict = {}
 
-    print(features)
+    for item in data:
+        data_dict[item]=[data[item]]
+
+    features = pd.DataFrame(data_dict)
+    for feature in ['sqft', 'zipcode', 'beds', 'stories', 'private_pool', 'lotsize', 'baths_count', 'year_built', 'remodeled_year']:
+        features[feature] = features[feature].astype('int32')
+
+    result = get_prediction(features)
+
     return render_template('form.html',
-                           #city=data['city'],
                            sqft=data['sqft'],
                            zipcode=data['zipcode'],
                            beds=data['beds'],
-                           #state=data['state'],
                            stories=data['stories'],
                            private_pool=data['private_pool'],
                            lotsize=data['lotsize'],
                            baths_count=data['baths_count'],
                            year_built=data['year_built'],
                            remodeled_year=data['remodeled_year'],
-                           result='test'
+                           result=result
                            )
-
 
 if __name__ == '__main__':
     app.run('localhost', 5000)
