2af2251b01e1d6e2a523e787e4ae99ff12d84c58
Sokolov-IM <56031152+Sokolov-IM@users.noreply.github.com>
2020-06-21 23:00:22 +0500

    Add files via upload

--- Дипломный проект/server/server.py
+++ Дипломный проект/server/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 import os
 import pandas as pd
-from house_prediction.application.house_predict import load_models, get_prediction
+from application.house_predict import load_models, get_prediction
 
 app = Flask(__name__)
 data_path = os.path.join(app.root_path, 'models')
@@ -16,16 +16,17 @@ def predict():
     data = request.form
     data_dict = {}
 
-    for item in data:
-        data_dict[item]=[data[item]]
+    try:
+        for item in data:
+            data_dict[item]=[data[item]]
 
-    features = pd.DataFrame(data_dict)
-    for feature in ['sqft', 'zipcode', 'beds', 'stories', 'private_pool', 'lotsize', 'baths_count', 'year_built', 'remodeled_year']:
-        features[feature] = features[feature].astype('int32')
+        features = pd.DataFrame(data_dict)
+        for feature in ['sqft', 'zipcode', 'beds', 'stories', 'private_pool', 'lotsize', 'baths_count', 'year_built']:
+            features[feature] = features[feature].astype('int32')
 
-    result = get_prediction(features)
+        result = get_prediction(features)
 
-    return render_template('form.html',
+        return render_template('form.html',
                            sqft=data['sqft'],
                            zipcode=data['zipcode'],
                            beds=data['beds'],
@@ -34,9 +35,20 @@ def predict():
                            lotsize=data['lotsize'],
                            baths_count=data['baths_count'],
                            year_built=data['year_built'],
-                           remodeled_year=data['remodeled_year'],
                            result=result
                            )
+    except Exception:
+        return render_template('form.html',
+                               sqft=data['sqft'],
+                               zipcode=data['zipcode'],
+                               beds=data['beds'],
+                               stories=data['stories'],
+                               private_pool=data['private_pool'],
+                               lotsize=data['lotsize'],
+                               baths_count=data['baths_count'],
+                               year_built=data['year_built'],
+                               result='нет_оценки'
+                               )
 
 if __name__ == '__main__':
     app.run('localhost', 5000)
